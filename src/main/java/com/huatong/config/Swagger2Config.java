80a748bfb57581b134172586c928cda13ed9c6d7
xiezongze <xiezz@yitkeji.com>
2018-11-26 13:38:26 +0800

    ddd

--- src/main/java/com/huatong/config/Swagger2Config.java
+++ src/main/java/com/huatong/config/Swagger2Config.java
@@ -1,5 +1,4 @@
 package com.huatong.config;
-import io.swagger.annotations.Api;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
